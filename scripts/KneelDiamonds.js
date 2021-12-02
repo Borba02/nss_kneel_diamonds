564b580797b5fc48fd55099e5cfd84c664c754a2
Bryce Borba <borbaesb@gmail.com>
2021-12-02 13:53:27 -0800

    added order state tracking and new order functionality

--- scripts/KneelDiamonds.js
+++ scripts/KneelDiamonds.js
@@ -1,17 +1,17 @@
+import { DiamondSizes } from "./DiamondSizes.js";
+import { JewelryStyles } from "./JewelryStyles.js";
+import { Orders } from "./Orders.js";
+import { Metals } from "./Metals.js";
+import { addCustomOrder } from "./database.js";
 
-import { DiamondSizes } from "./DiamondSizes.js"
-import { JewelryStyles } from "./JewelryStyles.js"
-import { Orders } from "./Orders.js"
-import { Metals } from "./Metals.js"
-
-document.addEventListener(
-    "click",
-    (event) => {
-    }
-)
+document.addEventListener("click", (event) => {
+  if (event.target.id === "orderButton") {
+    addCustomOrder();
+  }
+});
 
 export const KneelDiamonds = () => {
-    return `
+  return `
         <h1>Kneel Diamonds</h1>
 
         <article class="choices">
@@ -37,6 +37,5 @@ export const KneelDiamonds = () => {
             <h2>Custom Jewelry Orders</h2>
             ${Orders()}
         </article>
-    `
-}
-
+    `;
+};
--- scripts/database.js
+++ scripts/database.js
@@ -6,46 +6,67 @@
 
 */
 const database = {
-    styles: [
-        { id: 1, style: "Classic", price: 500 },
-        { id: 2, style: "Modern", price: 710 },
-        { id: 3, style: "Vintage", price: 965 }
-    ],
-    sizes: [
-        { id: 1, carets: 0.5, price: 405 },
-        { id: 2, carets: 0.75, price: 782 },
-        { id: 3, carets: 1, price: 1470 },
-        { id: 4, carets: 1.5, price: 1997 },
-        { id: 5, carets: 2, price: 3638 }
-    ],
-    metals: [
-        { id: 1, metal: "Sterling Silver", price: 12.42 },
-        { id: 2, metal: "14K Gold", price: 736.4 },
-        { id: 3, metal: "24K Gold", price: 1258.9 },
-        { id: 4, metal: "Platinum", price: 795.45 },
-        { id: 5, metal: "Palladium", price: 1241.0 }
-    ],
-    customOrders: [
-        {
-            id: 1,
-            metalId: 3,
-            sizeId: 2,
-            styleId: 3,
-            timestamp: 1614659931693
-        }
-    ],
-}
+  orderBuilder: {},
+  styles: [
+    { id: 1, style: "Classic", price: 500 },
+    { id: 2, style: "Modern", price: 710 },
+    { id: 3, style: "Vintage", price: 965 },
+  ],
+  sizes: [
+    { id: 1, carets: 0.5, price: 405 },
+    { id: 2, carets: 0.75, price: 782 },
+    { id: 3, carets: 1, price: 1470 },
+    { id: 4, carets: 1.5, price: 1997 },
+    { id: 5, carets: 2, price: 3638 },
+  ],
+  metals: [
+    { id: 1, metal: "Sterling Silver", price: 12.42 },
+    { id: 2, metal: "14K Gold", price: 736.4 },
+    { id: 3, metal: "24K Gold", price: 1258.9 },
+    { id: 4, metal: "Platinum", price: 795.45 },
+    { id: 5, metal: "Palladium", price: 1241.0 },
+  ],
+  customOrders: [
+    {
+      id: 1,
+      metalId: 3,
+      sizeId: 2,
+      styleId: 3,
+      timestamp: 1614659931693,
+    },
+  ],
+};
 
 export const getMetals = () => {
-    return database.metals.map(metal => ({...metal}))
-}
+  return database.metals.map((metal) => ({ ...metal }));
+};
 export const getSizes = () => {
-    return database.sizes.map(metal => ({...metal}))
-}
+  return database.sizes.map((metal) => ({ ...metal }));
+};
 export const getStyles = () => {
-    return database.styles.map(metal => ({...metal}))
-}
+  return database.styles.map((metal) => ({ ...metal }));
+};
 export const getOrders = () => {
-    return database.customOrders.map(metal => ({...metal}))
-}
+  return database.customOrders.map((metal) => ({ ...metal }));
+};
 
+export const setMetal = (id) => {
+  database.orderBuilder.metalId = id;
+};
+
+export const setSize = (id) => {
+  database.orderBuilder.sizeId = id;
+};
+
+export const setStyle = (id) => {
+  database.orderBuilder.styleId = id;
+};
+export const addCustomOrder = () => {
+  const newOrder = { ...database.orderBuilder };
+  const lastIndex = database.customOrders.length - 1;
+  newOrder.id = database.customOrders[lastIndex].id + 1;
+  newOrder.timestamp = Date.now();
+  database.customOrders.push(newOrder);
+  database.orderBuilder = {};
+  document.dispatchEvent(new CustomEvent("stateChanged"));
+};
